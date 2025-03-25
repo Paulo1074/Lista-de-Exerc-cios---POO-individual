2d453f66927b8c3497c3a5c6fdf0937322d91d9e
Paulo Otávio <pereirapaulootavio98@gmail.com>
2025-03-25 19:26:38 -0300

    Adicionar arquivos de projeto.

---
+++ Lista de Exercícios - POO - Individual/Program.cs
@@ -0,0 +1,10 @@
+﻿,namespace Lista_de_Exercícios___POO___Individual
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
